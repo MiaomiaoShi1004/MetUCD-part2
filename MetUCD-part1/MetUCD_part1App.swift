78fd316091ed25a6501d2a7d928edfd4ae4fcbf3
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-11-23 11:14:23 +0800

    Initial Commit

---
+++ MetUCD-part1/MetUCD_part1App.swift
@@ -0,0 +1,17 @@
+//
+//  MetUCD_part1App.swift
+//  MetUCD-part1
+//
+//  Created by Miaomiao Shi on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetUCD_part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
